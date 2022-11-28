d8dccbf8dc66fe1d5f1b5c725a9d3e8bb3fe58cb
Harry <harrytruong1772@gmail.com>
2022-11-28 22:40:46 +0700

    feat: product detail page

---
+++ src/pages/product-detail/page/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from '@chakra-ui/react'
+
+const index = () => {
+  return (
+    <div>
+        <Text fontSize='5xl'>
+            alo alo
+        </Text>
+    </div>
+  )
+}
+
+export default index
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import HomePage from 'pages/home-page/page/index';
-import TestPage from 'pages/test-page/page/index'
+import TestPage from 'pages/test-page/page/index';
+import ProductDetail from 'pages/product-detail/page/index';
 import { BasicRoute } from 'components/elements/AppRouter';
 import Layout from 'components/layouts/Layout';
 
@@ -11,6 +12,7 @@ export const routes: BasicRoute[] = [
         routes: [
             {path: '/', component: HomePage, exact: true},
             {path: '/test', component: TestPage, exact: true},
+            {path: '/product-detail', component: ProductDetail, exact: true}
         ]
     }
 ]
