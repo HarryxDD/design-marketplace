5e9fc911847f45dffbdd1c8df4663c86d7dbd203
MinhHung2703 <nguyenminhhung27032002@gmail.com>
2022-12-02 15:51:30 +0700

    Navbar-product

--- src/routes.ts
+++ src/routes.ts
@@ -3,14 +3,17 @@ import TestPage from 'pages/test-page/page/index'
 import { BasicRoute } from 'components/elements/AppRouter';
 import Layout from 'components/layouts/Layout';
 
+import Navbar from 'pages/navbar/nav';
+
 export const routes: BasicRoute[] = [
     {
         path: '/',
         component: Layout,
         exact: false,
         routes: [
-            {path: '/', component: HomePage, exact: true},
-            {path: '/test', component: TestPage, exact: true},
+            { path: '/', component: HomePage, exact: true },
+            { path: '/test', component: TestPage, exact: true },
+            { path: '/nav', component: Navbar },
         ]
     }
 ]
